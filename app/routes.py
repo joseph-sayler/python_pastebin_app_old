da758e90b6dfaa203db8250bedf9abd833ea9691
Joseph Sayler <joseph.sayler@gmail.com>
2021-08-26 23:09:30 -0700

    update flask routes
    update root route to allow post/get requests and to take in data and save it to database
    create new route that takes in an id and checks db to see if entry exists and then display that entry on the new results template page

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,28 @@
-from flask import render_template
+from secrets import token_urlsafe
+from flask import render_template, request, redirect, abort
+from app.models import Pastes
 from app import app
 
-@app.route("/")
+
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        title = request.form.get("title").strip()
+        paste_text = request.form.get("paste-text").strip()
+        identifier = token_urlsafe(5)
+        paste = Pastes(identifier=identifier,
+                       paste_text=paste_text,
+                       title=title)
+        paste.save()
+        return redirect(request.host_url + identifier)
     return render_template("index.html")
+
+
+@app.route("/<string:paste_id>/")
+def render_paste(paste_id):
+    try:
+        paste = Pastes.get(Pastes.identifier == paste_id)
+    except:
+        abort(404)
+
+    return render_template("results.html", paste=paste)
