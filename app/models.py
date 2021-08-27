aa7a443fa2038474d5e1877e795373fb7a0d1ed9
Joseph Sayler <joseph.sayler@gmail.com>
2021-08-26 22:19:16 -0700

    add orm models classes to access data from a sqlite database

---
+++ app/models.py
@@ -0,0 +1,18 @@
+import peewee
+import pytz
+from datetime import datetime
+
+db = peewee.SqliteDatabase("paste_db.db")
+
+class BaseModel(peewee.Model):
+    class Meta:
+        database = db
+
+class Pastes(BaseModel):
+    """
+    table to hold user pastes
+    """
+    identifier=peewee.TextField()
+    paste_text=peewee.TextField()
+    title=peewee.TextField()
+    date=peewee.DateTimeField(default=datetime.now(pytz.UTC))
