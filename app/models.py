34ad76157f31179dea95c9e4ac0a740d09b742af
Joseph Sayler <joseph.sayler@gmail.com>
2021-08-26 23:09:03 -0700

    update order of columns in orm model

--- app/models.py
+++ app/models.py
@@ -4,15 +4,17 @@ from datetime import datetime
 
 db = peewee.SqliteDatabase("paste_db.db")
 
+
 class BaseModel(peewee.Model):
     class Meta:
         database = db
+
 
 class Pastes(BaseModel):
     """
     table to hold user pastes
     """
-    identifier=peewee.TextField()
-    paste_text=peewee.TextField()
-    title=peewee.TextField()
-    date=peewee.DateTimeField(default=datetime.now(pytz.UTC))
+    identifier = peewee.TextField()
+    title = peewee.TextField()
+    paste_text = peewee.TextField()
+    date = peewee.DateTimeField(default=datetime.now(pytz.UTC))
