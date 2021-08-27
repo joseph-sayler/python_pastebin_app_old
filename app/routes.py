ac637eadd4621edf2d5a8ab39f539bfa051ab7d8
Joseph Sayler <joseph.sayler@gmail.com>
2021-08-26 20:51:29 -0700

    update route to use template

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route("/")
 def index():
-    return "Hello world"
+    title = "Home"
+    greeting = "Hello, World!"
+    return render_template("index.html", title=title, greeting=greeting)
