bff7b0c6c81ad4d6ca4f3b3bd3fd254c7b9ea55f
Joseph Sayler <joseph.sayler@gmail.com>
2021-08-26 23:08:40 -0700

    add flask cli command to create db and add pastes table

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+import click, peewee, os
 from flask import Flask
 from flask_bootstrap import Bootstrap
 
@@ -5,4 +6,13 @@ app = Flask(__name__)
 Bootstrap(app)
 
 # place at bottom to avoid circular imports
-from app import routes
+from app import routes, models
+
+@app.cli.command("create-pastes-table")
+def create_user():
+    """create the pastes_db.db file and add the Pastes table. does nothing if table already exists
+    """
+    try:
+        models.Pastes.create_table()
+    except peewee.OperationalError:
+        print("Pastes table already exists in database")
