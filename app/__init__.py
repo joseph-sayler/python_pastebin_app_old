d7e8676122709d40546174429d3f35154ad9d5c1
Joseph Sayler <joseph.sayler@gmail.com>
2021-08-26 21:21:37 -0700

    move routes import to bottom of file

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
-from app import routes
 from flask import Flask
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 Bootstrap(app)
+
+# place at bottom to avoid circular imports
+from app import routes
