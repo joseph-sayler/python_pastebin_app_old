a7caccdbdae7e9c35c22dbfec43e37a78dd7f769
Joseph Sayler <joseph.sayler@gmail.com>
2021-08-26 21:22:04 -0700

    remove hello world sample output text

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,4 @@ from app import app
 
 @app.route("/")
 def index():
-    title = "Home"
-    greeting = "Hello, World!"
-    return render_template("index.html", title=title, greeting=greeting)
+    return render_template("index.html")
