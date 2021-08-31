beb78c6a4f9f3ee1e6b72de4252b54ed33b5c127
Joseph Sayler <joseph.sayler@gmail.com>
2021-08-30 19:11:00 -0700

    add database type to config class

--- config.py
+++ config.py
@@ -8,3 +8,4 @@ load_dotenv(os.path.join(basedir, '.env'))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY', 'very-special-key')
     DATABASE = os.environ.get('DATABASE', None)
+    DATABASE_TYPE=os.environ.get('DATABASE_TYPE', None)
