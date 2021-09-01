ba828981fd919486e8323a88ac514769407a4ae6
Joseph Sayler <joseph.sayler@gmail.com>
2021-08-31 21:22:42 -0700

    separate database env vars into a dataclass

--- config.py
+++ config.py
@@ -1,11 +1,16 @@
 import os
 from dotenv import load_dotenv
+from dataclasses import dataclass
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+basedir = os.path.abspath(os.path.dirname("."))
 load_dotenv(os.path.join(basedir, '.env'))
+
+@dataclass
+class Database_config:
+    DATABASE = os.environ.get('DATABASE', None)
+    DATABASE_TYPE = os.environ.get('DATABASE_TYPE', None)
 
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY', 'very-special-key')
-    DATABASE = os.environ.get('DATABASE', None)
-    DATABASE_TYPE=os.environ.get('DATABASE_TYPE', None)
+
