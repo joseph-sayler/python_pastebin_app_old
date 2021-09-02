ffab06618cc687abe98e4090732b704ad77d8def
Joseph Sayler <joseph.sayler@gmail.com>
2021-09-01 18:06:16 -0700

    move Meta class from BaseClass to Pastes class and remove BaseClass

--- app/models.py
+++ app/models.py
@@ -4,21 +4,13 @@ from config import Database_config
 from app.database import Database
 
 
-db = Database(config=Database_config)
-
-
-class BaseModel(peewee.Model):
-    """A base model that uses a database object to determine which database to instantiate"""
-    class Meta:
-        database = db.database
-
-
-# db_wrapper.Model superclass allows app factory pattern to
-# be used to create the database; now the database info is
-# part of the flask configuration instead of stated here
-class Pastes(BaseModel):
+class Pastes(peewee.Model):
     """table to hold user pastes"""
     identifier = peewee.TextField()
     title = peewee.TextField()
     paste_text = peewee.TextField()
     date = peewee.DateTimeField(default=datetime.utcnow())
+
+    class Meta:
+        db = Database(config=Database_config)
+        database = db.database
