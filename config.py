7f79c91c671eae736819079eaae8cb649cee438b
Joseph Sayler <joseph.sayler@gmail.com>
2021-09-12 15:56:54 -0700

    move fauna specific config data to own dataclass

--- config.py
+++ config.py
@@ -9,10 +9,17 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
 
-# @dataclass
-# class Database_config:
-#     DATABASE = os.environ.get('DATABASE', None)
-#     DATABASE_TYPE = os.environ.get('DATABASE_TYPE', None)
+@dataclass
+class Database_config:
+    DATABASE = os.environ.get('DATABASE', None)
+    DATABASE_TYPE = os.environ.get('DATABASE_TYPE', None)
+
+@dataclass
+class Fauna_config:
+    FAUNA_SECRET_KEY = os.environ.get("FAUNA_SECRET_KEY", None)
+    FAUNA_DOMAIN = os.environ.get("FAUNA_DOMAIN", None)
+    FAUNA_COLLECTION = os.environ.get("FAUNA_COLLECTION", None)
+    FAUNA_INDEX = os.environ.get("FAUNA_INDEX", None)
 
 
 class Config(object):
