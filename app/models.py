147d8fe9d0d7437f3d64cfa36461058cec711f18
Joseph Sayler <joseph.sayler@gmail.com>
2021-09-12 18:54:13 -0700

    update where db is created and how models react to this
    was running into issue where when trying to use fauna, the SQL_Pastes model was trying to initialize using db.Model; Fauna_DB() has no access to Model class so this was failing; by initializing db here, the need for Fauan_DB to have a dependency on Model was removed

--- app/models.py
+++ app/models.py
@@ -1,33 +1,40 @@
-from app import db
 from datetime import datetime
 from config import TOKEN_SIZE
+from config import Database_config
+from flask_sqlalchemy import SQLAlchemy
+from app.database import Fauna_DB
 
 
-class SQL_Pastes(db.Model):
-    """table to hold user pastes"""
+if Database_config.DATABASE_TYPE == "FAUNA":
+    db = Fauna_DB()
 
-    identifier = db.Column(db.String(TOKEN_SIZE),
-                           nullable=False, unique=True, primary_key=True)
-    title = db.Column(db.Text, nullable=False)
-    paste_text = db.Column(db.Text, nullable=False)
-    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow())
+    class Fauna_Pastes:
+        """table to hold user pastes"""
 
-    def __repr__(self):
-        return f'<Paste {self.identifier}>'
+        def __init__(self, identifier=None, title=None, paste_text=None, date=None):
+            self.identifier = identifier
+            self.title = title
+            self.paste_text = paste_text
+            self.date = date if date else datetime.utcnow()
 
+        def _from_dict(self, data):
+            for key, value in data.items():
+                setattr(self, key, value)
 
-class Fauna_Pastes:
-    """table to hold user pastes"""
+        def __repr__(self):
+            return f'<Paste {self.identifier}>'
+else:  # == "SQLITE"
+    db = SQLAlchemy()
 
-    def __init__(self, identifier=None, title=None, paste_text=None, date=None):
-        self.identifier = identifier
-        self.title = title
-        self.paste_text = paste_text
-        self.date = date if date else datetime.utcnow()
+    class SQL_Pastes(db.Model):
+        """table to hold user pastes"""
 
-    def _from_dict(self, data):
-        for key, value in data.items():
-            setattr(self, key, value)
+        identifier = db.Column(db.String(TOKEN_SIZE),
+                               nullable=False, unique=True, primary_key=True)
+        title = db.Column(db.Text, nullable=False)
+        paste_text = db.Column(db.Text, nullable=False)
+        date = db.Column(db.DateTime, nullable=False,
+                         default=datetime.utcnow())
 
-    def __repr__(self):
-        return f'<Paste {self.identifier}>'
+        def __repr__(self):
+            return f'<Paste {self.identifier}>'
